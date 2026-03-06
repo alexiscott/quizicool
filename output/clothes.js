17f65b92ab2cd52927fbde5a6355044a1970008e
Alex scott <arkima@alexscott.net>
2026-03-06 19:25:19 +0100

    Example output

---
+++ output/clothes.js
@@ -0,0 +1,7 @@
+const exerciseConfig = {
+      wordsData: [
+        {image: "images/legs.jpg", word: "Jeans and things"},
+        {image: "images/jump.aiff", word: "Jumpers are great"}
+      ],
+      classes: {  dropZoneContainer: "drop-zones",  dropItem: "item",  dropZone: "drop-zone",  image: "image",  word: "word"}
+    };
---
+++ output/food.js
@@ -0,0 +1,8 @@
+const exerciseConfig = {
+      wordsData: [
+        {image: "images/pizza", word: "Pizza"},
+        {image: "images/bread", word: "Bread"},
+        {image: "images/eggs", word: "Eggs"}
+      ],
+      classes: {  dropZoneContainer: "drop-zones",  dropItem: "item",  dropZone: "drop-zone",  image: "image",  word: "word"}
+    };
